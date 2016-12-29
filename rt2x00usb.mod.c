84d6d3a592b34e7808d5976df2f9667801587c1b
Lyuben Deninski <ldeninski@gmail.com>
2016-12-30 01:10:40 +0200

    initial commit

---
+++ rt2x00usb.mod.c
@@ -0,0 +1,21 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.arch = MODULE_ARCH_INIT,
+};
+
+MODULE_INFO(intree, "Y");
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=rt2x00lib,mac80211";
+
+
+MODULE_INFO(srcversion, "F4D1AD5BD6BED1AA266A670");
